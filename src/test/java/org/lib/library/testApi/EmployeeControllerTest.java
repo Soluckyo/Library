91e53aad817749baf019ae52c58c9cad62026317
Soluckyo <twinki0000@yandex.ru>
2025-02-23 23:41:43 +0300

    create jar

--- src/test/java/org/lib/library/testApi/EmployeeControllerTest.java
+++ src/test/java/org/lib/library/testApi/EmployeeControllerTest.java
@@ -1,31 +1,45 @@
 package org.lib.library.testApi;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.lib.library.api.EmployeeController;
 import org.lib.library.entity.Book;
 import org.lib.library.service.BookService;
-import org.lib.library.service.EmployeeService;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(MockitoExtension.class)
+@Disabled
+@ExtendWith({MockitoExtension.class, SpringExtension.class})
+@WebMvcTest(EmployeeController.class)
+@Import(BookService.class)
 public class EmployeeControllerTest {
 
     @Mock
     private BookService bookService;
 
-    @InjectMocks
+    private MockMvc mockMvc;
+
+    @Autowired
     private EmployeeController employeeController;
 
     private Book book;
@@ -33,14 +47,15 @@ public class EmployeeControllerTest {
     @BeforeEach
     void setUp() {
         book = new Book();
-        book.setIsbn("123");
-        book.setName("John Doe");
-        book.setAuthor("John Doe");
+        book.setIsbn("12345");
+        book.setName("Test Book");
+        book.setAuthor("Test Author");
+        book.setYearOfPublication("2021");
         book.setAvailable(true);
     }
 
     @Test
-    public void testAddBook(){
+    public void testAddBook() throws Exception {
         doNothing().when(bookService).addBook(book);
 
         ResponseEntity<String> response = employeeController.addBook(book);
@@ -51,5 +66,16 @@ public class EmployeeControllerTest {
         assertEquals(expectedMessage, response.getBody());
 
         verify(bookService, times(1)).addBook(book);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/v1/employees/new_book")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"isbn\":\"12345\"," +
+                        "\"name\":\"Test Book\"," +
+                        "\"author\":\"Test Author\"," +
+                        "\"yearOfPublication\":\"2021\"," +
+                        "\"isAvailable\":\"true\"}"))
+                .andExpect(status().isCreated())
+                .andExpect(MockMvcResultMatchers.content().string("Book [isbn=12345, name=Test Book, author=Test Author]Книга успешно добавлена"));
+
     }
 }
